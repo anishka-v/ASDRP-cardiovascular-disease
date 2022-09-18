c47a07f768aa9b294bdbbebb09fc79e89f3090eb
Anishka Vissamsetty <86209160+anishka-v@users.noreply.github.com>
2022-09-18 13:38:51 -0700

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_curve, auc
 
+#MODEL
 
 features = ["age","gender","height","weight", "ap_hi","ap_lo","gluc","cholesterol", "smoke", "active","alco"]
 X = df.loc[:, features]
@@ -23,4 +24,50 @@ cm
 feature_imp = pd.Series(clf.feature_importances_, index = features).sort_values(ascending = False)
 print (feature_imp)
  
+#HYPERPARAMETER TUNING 
 
+#number of estimators 
+n_estimators = [1, 2, 4, 8, 16, 32, 64, 100, 200]
+train_results = []
+test_results = []
+for estimator in n_estimators:
+   rf = RandomForestClassifier(n_estimators=estimator, n_jobs=-1)
+   rf.fit(X_train, y_train)
+   train_pred = rf.predict(X_train)
+   false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train, train_pred)
+   roc_auc = auc(false_positive_rate, true_positive_rate)
+   train_results.append(roc_auc)
+   y_pred = rf.predict(X_test)
+   false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
+   roc_auc = auc(false_positive_rate, true_positive_rate)
+   test_results.append(roc_auc)
+from matplotlib.legend_handler import HandlerLine2D
+line1, = plt.plot(n_estimators, train_results, "b", label="Train AUC")
+line2, = plt.plot(n_estimators, test_results, "r", label="Test AUC")
+plt.legend(handler_map={line1: HandlerLine2D(numpoints=2)})
+plt.ylabel("AUC score")
+plt.xlabel("n_estimators")
+plt.show()
+
+#max depth of the tree
+max_depths = np.linspace(1, 32, 32, endpoint=True)
+train_results = []
+test_results = []
+for max_depth in max_depths:
+   rf = RandomForestClassifier(max_depth=max_depth, n_jobs=-1)
+   rf.fit(X_train, y_train)
+   train_pred = rf.predict(X_train)
+   false_positive_rate, true_positive_rate, thresholds = roc_curve(y_train, train_pred)
+   roc_auc = auc(false_positive_rate, true_positive_rate)
+   train_results.append(roc_auc)
+   y_pred = rf.predict(X_test)
+   false_positive_rate, true_positive_rate, thresholds = roc_curve(y_test, y_pred)
+   roc_auc = auc(false_positive_rate, true_positive_rate)
+   test_results.append(roc_auc)
+from matplotlib.legend_handler import HandlerLine2D
+line1, = plt.plot(max_depths, train_results, "b", label="Train AUC")
+line2, = plt.plot(max_depths, test_results, "r", label="Test AUC")
+plt.legend(handler_map={line1: HandlerLine2D(numpoints=2)})
+plt.ylabel("AUC score")
+plt.xlabel("Tree depth")
+plt.show()
